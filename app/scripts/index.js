2a7af33ad0b7263ae7244e115cc639eb09b07a30
yunsong <yunsong.mao@gmail.com>
2014-09-25 18:08:58 +0200

    test for ractive js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -6,3 +6,14 @@ log.error('test for log error');
 var App = require('./app');
 
 new App();
+
+//test Ractive
+var Ractive = require('ractive');
+
+var ractive = new Ractive({
+  el: document.getElementById('results'),
+  template: "{{greeting}} {{name}}",
+  data: { greeting: 'Hello', name: 'world !!!' }
+});
+
+
